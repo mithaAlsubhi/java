701405cd87aa4560c8ccaa1c4e037c5dcc6b93c6
mithaAlsubhi <138530744+mithaAlsubhi@users.noreply.github.com>
2023-07-26 16:29:59 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,277 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       /* System.out.println("mitha");
+        System.out.println(199);
+        System.out.println(77.7);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(1 + 2);
+        System.out.println(5 - 2);
+        System.out.println(6 * 4);
+        System.out.println(5 / 2);
+        System.out.println(5.0 / 2);
+        System.out.println(6 > 7);
+        System.out.println(8 < 10);
+        System.out.println(7 == 5);
+        System.out.println(3 != 7);
+        System.out.println("J a v a \n");
+        System.out.println(" --- --- ---");
+        System.out.println(" |   |   | ");
+        System.out.println("  --- --- ---");
+        System.out.println(" |   |   | ");
+        System.out.println("  --- --- ---");
+        System.out.println(" |   |   | ");
+        System.out.println("  --- --- ---");
+        System.out.println(4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+        System.out.println("\n o o o m m m m m m m");
+        System.out.println("\n o o o m m m m m m m");
+        System.out.println("\n o o o a a a a a a a");
+        System.out.println("\n o o o a a a a a a a");
+        System.out.println("\n o o o n n n n n n n");
+        System.out.println("\n o o o n n n n n n n");
+        //
+        /*int x=100;
+        x=x+200;
+        double y=10.5;
+        String name = "mitha";
+        boolean isMaried =true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(name);
+        System.out.println(isMaried);
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("please enter yor name");
+       String name = read.next();
+        System.out.println("welcome " + name);
+        System.out.println("please enter your age");
+        int x = read.nextInt();
+        System.out.println("age :" +x);
+        int y=100;
+        System.out.println("You have"+(y-x)+"years to reach 100");
+        System.out.println(" please enter number1");
+        double d = read.nextDouble();
+        System.out.println("please enter number2");
+        double e= read.nextDouble();
+        System.out.println(d+e);
+        System.out.println(d-e);
+        System.out.println(d*e);
+        System.out.println(d/e);
+
+        System.out.println("enter the radius of circle");
+        double r = read.nextDouble();
+        double perimeter = 2 * Math.PI * r;
+        double Area = Math.PI * r * r;
+        System.out.println("perimeter is = " +perimeter);
+        System.out.println("Area is = " +Area);
+
+
+        System.out.println("enter the number 1");
+        double m = read.nextDouble();
+        System.out.println("enter the number 2");
+        double n = read.nextDouble();
+        System.out.println("enter the number 3");
+        double l = read.nextDouble();
+        double average = ( m + n + l /3);
+        System.out.println(" Average is" +average);
+
+        System.out.println("enter the Width of rectangle");
+        double w = read.nextDouble();
+        System.out.println("enter the Height of rectangle");
+        double h = read.nextDouble();
+        double area = w * h ;
+        double Perimeter = (w + h) *2;
+        System.out.println("perimeter is = " +Perimeter);
+        System.out.println("Area is = " +area);
+
+
+        System.out.println("/n");
+        Scanner swap = new Scanner(System.in);
+        System.out.println("enter k :");
+        double k=swap.nextDouble();
+        System.out.println("enter v :");
+        double v=swap.nextDouble();
+        System.out.println("before swaping"+k+",+"+v);
+        double x;
+        k=v;
+        v=k;
+        k=x;
+        System.out.println("after swaping"+k+",+"+v);*/
+
+
+
+
+
+
+         /*int x=10;
+         int y=5;
+        System.out.println(10>7&&x<15||x==y);
+        System.out.println(!(x < 15) && x < 5 || x > y);
+        System.out.println(true && true || false || true && false);
+        System.out.println(!(!(true && false)|| false )|| true && false || true);
+        if(x > y){
+            System.out.println("x is larger than y");
+        } else if(x==y) {
+            System.out.println("equal");
+
+        }
+        else {
+            System.out.println("error");
+        }
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("enter the number 1");
+        int a =read.nextInt();
+        System.out.println("enter the number 2");
+        int b =read.nextInt();
+        System.out.println("enter the number 3");
+        int c =read.nextInt();
+        if((a>b) && (a>c)){
+            System.out.println(" larger is " +a);
+        } else if((b>a) && (b>c)){
+            System.out.println(" larger is " +b);
+        }
+        else if((c>a) && (c>b)){
+            System.out.println(" larger is " +c);
+        }*/
+
+       /* Scanner read = new Scanner(System.in);
+        System.out.println("enter your mark1 ");
+        int m1 =read.nextInt();
+        System.out.println("enter the mark2");
+        int m2 =read.nextInt();
+        System.out.println("enter the mark3");
+        int m3 =read.nextInt();
+        System.out.println("enter the mark4");
+        int m4 =read.nextInt();
+        System.out.println("enter the mark5");
+        int m5 =read.nextInt();
+        int average = ( m1+ m2 + m3 + m4 +m5)/5;
+        System.out.println(" Average is " +average);
+        if((average<=100)&&(average>=90)){
+            System.out.println(" Your grade A");
+        }
+        else if((average<=89)&&(average>=80)){
+            System.out.println("Your grade B");
+        }
+        else if((average<=79)&&(average>=70)){
+            System.out.println("Your grade C");
+        }
+        else if ((average<=69)&&(average>=60)){
+            System.out.println("Your grade D");
+        }
+        else {
+            System.out.println("You are fail");
+        }*/
+
+        /*Scanner read = new Scanner(System.in);
+        System.out.println("enter your number1 ");
+        int n1 =read.nextInt();
+        System.out.println("enter the number2");
+        int n2 =read.nextInt();
+        System.out.println("please chose operator ");
+        String op= read.next();
+        if (op.equals("+")){
+
+            System.out.println("your result =" +n1 + "+" +n2 +"=" +(n1+n2));
+
+        }
+        else if(op.equals("-")){
+            System.out.println("your result =" +n1 + "-" +n2 +"=" +(n1-n2)) ;
+        }
+        else if(op.equals("*")){
+            System.out.println("your result =" +n1 + "*" +n2 +"=" +(n1*n2)) ;
+        }
+        else if(op.equals("/")){
+            System.out.println("your result =" +n1 + "/" +n2 +"=" +(n1/n2)) ;*/
+
+
+        /*for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+                int n = 1;
+                while (n >= 100) {
+                    System.out.println(n);
+                    if (i % 2 != 0) {
+
+                    }*/
+
+                 /*nt i=0;
+                    do {
+
+                    if(
+                            i != 1
+                            && i % 2 !=0
+                            && i % 3 !=0
+                            && i % 5 !=0
+                            && i % 7 !=0
+                            || i == 2
+                            || i == 3
+                            || i == 5
+                            || i == 7
+                    ){
+                        System.out.println(i);
+
+                    }
+                    i++;
+                }while (i<=100);*/
+     /*  System.out.println("please enter yor name");
+        System.out.println("please enter number of question");
+        int number = read.nextInt();
+        String[][] questions = {
+                {"question1:2+2?"},
+                {"question2:5-2?"},
+                {"question3:1*1?"},
+                {"question4:4+4?"},
+                {"question:5*5?"}};
+        int[] answer = {
+                4,
+                3,
+                1,
+                8,
+                25
+        };
+        int score = 0;
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            System.out.println("please enter your answer:");
+            */
+
+
+            String[] word = {"hello", "in", "our", "new", "word"};
+            String space= " ";
+            String star = "*";
+            String row = "";
+
+            String firstRow="";
+            int maxLength= word[0].length();
+            for (int i = 0; i < word.length; i++) {
+                if (word[i].length()>maxLength) {
+                    maxLength = word[i].length();
+
+                }
+
+                }
+            firstRow= star.repeat(maxLength + 2);
+        System.out.println(firstRow);
+        for(int i=0;i< word.length;i++) {
+            row = "*" + word[i];
+            row = row + space.repeat(maxLength - word[i].length());
+            row = row + "*";
+            System.out.println(row);
+        }
+        System.out.println(firstRow);
+
+
+            }
+
+
+
+
+            }
+
